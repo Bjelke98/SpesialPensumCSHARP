e2bdaf23da2b0ec8d156ede3af7d0f698f0f288c
Krister Iversen <krister.iversen@outlook.com>
2022-10-16 03:19:49 +0200

    Add project files.

---
+++ FlatUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using FlatUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FlatUI.MVVM.ViewModel {
+    class MainViewModel : ObservableObject {
+
+		public RelayCommand HomeViewCommand{ get; set; }
+		public RelayCommand DiscoveryViewCommand{ get; set; }
+		public RelayCommand CloseWindowCommand{ get; set; }
+		public RelayCommand MinimizeWindowCommand{ get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+		public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+		public object CurrentView {
+			get { return _currentView; }
+			set { 
+				_currentView = value; 
+				OnPropertyChanged();
+			}
+		}
+
+
+		public MainViewModel() {
+			HomeVM = new HomeViewModel();
+			DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+			HomeViewCommand = new RelayCommand(o => {
+				CurrentView = HomeVM;
+			});
+            DiscoveryViewCommand = new RelayCommand(o => {
+                CurrentView = DiscoveryVM;
+            });
+            CloseWindowCommand = new RelayCommand(o => {
+                var windowFacade = o as IWindowFacade;
+                windowFacade?.Close();
+            });
+            MinimizeWindowCommand = new RelayCommand(o => {
+                var windowFacade = o as IWindowFacade;
+                windowFacade?.Minimize();
+            });
+
+        }
+
+    }
+}
---
+++ FlatUI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using FlatUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlatUI {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IWindowFacade {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        public void Minimize() {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MoveWindow(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                this.DragMove();
+            }
+        }
+    }
+}
