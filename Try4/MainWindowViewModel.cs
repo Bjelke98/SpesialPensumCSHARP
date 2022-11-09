f73ba68570962e5cea48f585fd51fcf3a0c5c477
Krister Iversen <krister.iversen@outlook.com>
2022-11-09 22:02:48 +0100

    Try#4

---
+++ Try4/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Try4 {
+    public partial class MainWindowViewModel : ObservableObject {
+
+        [ObservableProperty]
+        private string firstName = "Bjelke";
+
+        public ICommand ClickCommand { get; }
+
+        public MainWindowViewModel() {
+            ClickCommand = new RelayCommand(OnClick);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnClick() {
+            FirstName = "Not Bjelke";
+        }
+
+    }
+}
